5b4d504426402d3460d0d47038485fabd7f6f822
caiquemiranda <caiquemirandas@gmail.com>
2025-01-23 22:34:53 -0300

    Update Iac2.sh to download application files from the new repository URL

--- scripts-ssh/Iac2.sh
+++ scripts-ssh/Iac2.sh
@@ -10,7 +10,7 @@ apt-get install unzip -y
 echo "Baixando e copiando os arquivos da aplicação..."
 
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+wget https://github.com/caiquemiranda/learning-linux/blob/main/linux-site.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
